37dc40962c37bfd748e61ee2038850bc317f9619
sairam427 <ramreddy.rachamalla@creditvidya.com>
2023-01-30 12:12:23 +0530

    Initial commitss

---
+++ app/src/main/java/com/example/simplelibrary/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.simplelibrary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Surface
+import com.example.developer.Sydney
+import com.example.developer.ToastMessage
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
---
+++ developer/src/main/java/com/example/developer/Sydney.kt
@@ -0,0 +1,5 @@
+package com.example.developer
+
+object Sydney {
+    val suburbs = listOf("Ryde", "Chippendale")
+}
