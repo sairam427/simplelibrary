f11c69d4870bd8313a23b423bde60a0dcad3e00c
sairam427 <ramreddy.rachamalla@creditvidya.com>
2023-01-31 16:19:11 +0530

    webview implemented

--- developer/src/main/java/com/example/developer/LibraryActivity.kt
+++ developer/src/main/java/com/example/developer/LibraryActivity.kt
@@ -4,10 +4,18 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class LibraryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_library)
+        var webview=findViewById<WebView>(R.id.webView)
+
+        webview.webViewClient = WebViewClient()
+        webview.loadUrl("https://www.google.com")
+        webview.settings.javaScriptEnabled = true
+        webview.settings.setSupportZoom(true)
     }
 }
